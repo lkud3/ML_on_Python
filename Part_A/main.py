4e40fd823090dc33feab8e57af706a7b670d32d8
Saltykov Daniil <d.saltykovme@gmail.com>
2024-05-21 20:44:41 +0300

    Finish of some TODOS and finalization of 3 option. 4 option is almost done

--- Part_A/main.py
+++ Part_A/main.py
@@ -24,6 +24,31 @@ def display_data(data_input) -> None:
     print(tabulate(data_input, headers='keys'))
 
 
+# Convert time in format mm:ss.ms to a double.
+def time_to_double(time_str):
+    minutes, seconds_ds = time_str.split(':')
+    seconds, deciseconds = seconds_ds.split('.')
+
+    # Calculates the final time in seconds and return the double variable
+    total_seconds = int(minutes) * 60 + int(seconds) + int(deciseconds) / 10
+    return total_seconds
+
+
+# Convert a double to time in format mm:ss.ms.
+def double_to_time(time_double):
+    minutes = int(time_double // 60)
+    seconds = int(time_double % 60)
+    deciseconds = round((time_double - minutes * 60 - seconds) * 10)
+
+    # In case of rounding deciseconds up to 10, increase seconds by one integer
+    if deciseconds == 10:
+        deciseconds = 0
+        seconds += 1
+
+    # Aggregates the final time and return the string with the same format as the initial one
+    return f"{minutes:02}:{seconds:02}.{deciseconds}"
+
+
 def read_data():
     print("--------------------------------------------------------------------------")
     global data
@@ -34,29 +59,44 @@ def read_data():
 
 def lap_search():
     print("--------------------------------------------------------------------------")
-    limit = int(input("Enter the limit of laps to search by: "))
-    # TODO("Error input catching")
+    # TODO(Redo the try catch shit)
+    # while True:
+    #     limit = input("Enter the limit of laps to search by: ")
+    #
+    #     if isinstance(limit, int):
+    #         if limit > 0:
+    #             break
+    #
+    #     print("Invalid input. Please try again!")
+
     display_data(data[data['LAPS'] >= limit].sort_values('GRAND PRIX'))
     input("Press Enter to continue...")
 
 
 def avg_lap_time():
     global data
-    print("-----------------------------------------------------------------")
-    # TODO("Parse the time into INT")
-    data['AVERAGE LAP TIME'] = data['TIME'] / data['LAPS']
-    data.to_csv('res/partA_output_data.txt', sep='\t', index=False)
-    display_data(pd.read_csv('res/partA_output_data.txt', sep='\t'))
+    print("--------------------------------------------------------------------------")
+    # Creating new column and populating it with new time in the same form as the initial one
+    data['AVERAGE LAP TIME'] = data['TIME'].apply(time_to_double) / data['LAPS']
+    data['AVERAGE LAP TIME'] = data['AVERAGE LAP TIME'].apply(double_to_time)
+
+    # Writing the new data frame into new file and displaying the results by reading from this new file
+    data.to_csv('res/partA_output_data.txt', sep=',', index=False)
+    display_data(pd.read_csv('res/partA_output_data.txt'))
     input("Press Enter to continue...")
 
 
 def field_sort():
-    print("-----------------------------------------------------------------")
+    print("--------------------------------------------------------------------------")
+    # TODO("Make the validation")
+    field = input("Enter the field to sort by: ")
+    order = input("Enter the order (ascending or descending): ")
+    # df.sort_values(field, ascending=order.lower() == 'ascending')
     input("Press Enter to continue...")
 
 
 def column_graph():
-    print("-----------------------------------------------------------------")
+    print("--------------------------------------------------------------------------")
     input("Press Enter to continue...")
 
 
@@ -66,7 +106,7 @@ def main():
     print("=================================================================")
 
     while True:
-        print("-----------------------------------------------------------------")
+        print("--------------------------------------------------------------------------")
         pi.display_menu_instructions()
         option = input("Enter your choice: ")
 
@@ -82,11 +122,15 @@ def main():
             case '3':
                 avg_lap_time()
             case '4':
+                if 'AVERAGE LAP TIME' not in data.columns:
+                    print("Not enough data. Please execute option 3 first.")
+                    input("Press Enter to continue...")
+                    continue
                 field_sort()
             case '5':
                 column_graph()
             case '6':
-                print("-----------------------------------------------------------------")
+                print("--------------------------------------------------------------------------")
                 print("You selected option 6.\nIt's time to say goodbye then...\nBye!")
                 break
             case default:
