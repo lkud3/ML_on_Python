7820bc3bf486885d823cd9b3210c546e6c75374e
Saltykov Daniil <d.saltykovme@gmail.com>
2024-06-05 14:28:32 +0300

    All options are done. The 6 option with all combination statistics is to be done. Some TODOs to increase stability and effectiviness are to be done as well

--- Part_B/main.py
+++ Part_B/main.py
@@ -10,17 +10,140 @@
 import os
 from tabulate import tabulate
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import common.print_instructions as pi
-import re
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+# TODO(save all the new data into files)
+
+
+def read_data(file, names):
+    print("--------------------------------------------------------------------------------------------------")
+    print(f"Reading the data from file {file}...")
+    data = pd.read_csv(file, names=names)
+    print("\nPrinting the first few lines of the data set:\n", tabulate(data.head(), headers='keys'))
+
+    data_variables = data.iloc[:, 0:4]
+    data_labels = data.select_dtypes(include=['int64'])
+    print("\nPrinting the labels column categories: ", data_labels.Class.unique(), "\n")
+
+    input("Press Enter to continue...")
+    return data_variables, data_labels
+
+
+# TODO(add default topology)
+def topology_choice():
+    print("--------------------------------------------------------------------------------------------------")
+    while True:
+        try:
+            # Receives user input and catches non-integer input
+            input_string = input("Enter the size of the hidden layers of the MLP topology (e.g. 10-10-10).\nHINT: "
+                                 "input and output layers are fixed, choose the size of hidden"
+                                 " layers only: ").replace(" ", "")
+
+            topology = tuple(int(x) for x in input_string.split("-") if x != "")
+
+            if len(topology) < 2:
+                raise ValueError("The MLP should have at least two hidden layers. Please try again!")
+
+            if any(x == 0 for x in topology):
+                raise ValueError("The hidden layer can not consist of zero nodes. Please try again!")
+
+            print(f"\nThe topology for hidden layers {input_string} has been chosen.")
+            input("Press Enter to continue...")
+            return topology
+
+        except ValueError as e:
+            print(f"\nError: {e}\n")
+
+
+def training_step_choice():
+    print("--------------------------------------------------------------------------------------------------")
+    while True:
+        # Receives user input and catches non-integer input
+        step = input(f"Enter the size of the training step (0.001 - 0.5, [ENTER] for adaptable): ").replace(" ", "")
+
+        if step == "":
+            print(f"\nThe learning step will be adaptable.")
+            input("Press Enter to continue...")
+            return 'adaptable'
+
+        try:
+            step = float(step)
+
+            if 0.001 <= step <= 0.5:
+                print(f"\nThe learning step of {step} has been chosen.")
+                input("Press Enter to continue...")
+                return step
+            else:
+                print("\nThe learning step should be between 0.001 and 0.5. Please try again!")
+
+        except ValueError:
+            print("\nInvalid input! Please enter a valid double value. Please try again!")
+
+
+def plot_training_error(mlp, entry_train, labels_train, epochs):
+    print("The error graph is building...")
+    errors = []
+    for i in range(epochs):
+        mlp.partial_fit(entry_train, labels_train.values.ravel())
+        errors.append(mlp.loss_)
+    plt.plot(range(epochs), errors)
+    plt.xlabel('Epochs')
+    plt.ylabel('Training Error')
+    plt.title('Training Error Curve')
+    print("\nThe graph is on the screen. Please close the window in order to proceed...")
+    plt.show()
+
+
+def train_mlp(entry_train, labels_train, topology, step):
+    print("--------------------------------------------------------------------------------------------------")
+    if isinstance(step, str):
+        mlp = MLPClassifier(hidden_layer_sizes=topology, max_iter=1000, learning_rate='adaptive')
+    else:
+        mlp = MLPClassifier(hidden_layer_sizes=topology, max_iter=1000, learning_rate_init=step)
+
+    mlp.fit(entry_train, labels_train.values.ravel())
+
+    plot_training_error(mlp, entry_train, labels_train, 1000)
+
+    print("\nThe mlp is trained.")
+    input("Press Enter to continue...")
+
+    return mlp
+
+
+def mpl_prediction(mlp, values_test, labels_test) -> None:
+    print("--------------------------------------------------------------------------------------------------")
+    predictions = mlp.predict(values_test)
+
+    print('\nNow printing the confusion matrix (without normalization)...\n')
+    print(confusion_matrix(labels_test, predictions))
+    print('\nNow printing the classification report...\n')
+    print(classification_report(labels_test, predictions))
+    print('\nNow printing the accuracy score...')
+    print(accuracy_score(labels_test, predictions))
+    input("Press Enter to continue...")
 
 
 # The main function
 def main():
-    # TODO(change the header)
-    print("=====================================================================")
-    print("USER MENU: MLP CLASSIFICATION OF THE ###### DATA SET (UCI REPOSITORY)")
-    print("=====================================================================")
+    file = 'res/data_banknote_authentication.txt'
+    names = ['variance', 'skewness', 'curtosis', 'entropy', 'Class']
+    labels = pd.DataFrame()
+    entry_data = pd.DataFrame()
+    entry_test = pd.DataFrame()
+    labels_test = pd.DataFrame()
+    topology = tuple()
+    step = 0.0
+
+    print("=======================================================================================")
+    print("USER MENU: MLP CLASSIFICATION OF THE BANK NOTE IDENTIFICATION DATA SET (UCI REPOSITORY)")
+    print("=======================================================================================")
 
     while True:
         print("--------------------------------------------------------------------------------------------------")
@@ -28,9 +151,11 @@ def main():
         option = input("Enter your choice: ").replace(" ", "")
 
         # Checks if the user use option 1 first to read the data
-        if option != '1' and option != '6':
+        if option != '1' and option != '7' and labels.empty:
             print("\nNot enough data to proceed! Please run \"1\" option to load the data first.")
             input("Press Enter to continue...")
+
+        # TODO(make limitations for option 4 (calling before 2 and 3), 5 should go after 4 )
         else:
             # Match case for menu implementation
             match option:
@@ -39,19 +164,29 @@ def main():
                     pi.mlp_legend()
                     input("Press Enter to continue...")
                 case '1':
-                    print()
+                    entry_data, labels = read_data(file, names)
                 case '2':
-                    print()
+                    topology = topology_choice()
                 case '3':
-                    print()
+                    step = training_step_choice()
                 case '4':
-                    print()
+                    entry_train, entry_test, labels_train, labels_test = train_test_split(entry_data, labels,
+                                                                                          test_size=0.20)
+                    scaler = StandardScaler()
+                    scaler.fit(entry_train)
+
+                    entry_train = scaler.transform(entry_train)
+                    entry_test = scaler.transform(entry_test)
+
+                    mlp = train_mlp(entry_train, labels_train, topology, step)
                 case '5':
-                    print()
+                    mpl_prediction(mlp, entry_test, labels_test)
                 case '6':
+                    print()
+                case '7':
                     print("-------------------------------------------------------------------------------------------"
                           "-------")
-                    print("You selected option 6.\nIt's time to say goodbye then...\nBye!")
+                    print("You selected option 7.\nIt's time to say goodbye then...\nBye!")
                     quit(0)
                 # The default case for wrong data input
                 case _:
--- common/print_instructions.py
+++ common/print_instructions.py
@@ -51,28 +51,28 @@ The order options:
 def mlp_menu_instructions():
     print("""
 1. Read the labelled text data file, display the first 5 lines
-2. Choose the size of the hidden layers of the MLP topology (e.g. 6-?-?-2)
+2. Choose the size of the hidden layers of the MLP topology (e.g. 4-?-?-1)
 3. Choose the size of the training step (0.001 - 0.5, [ENTER] for adaptable)
 4. Train on 80% of labeled data, display progress graph
 5. Classify the unlabeled data, output training report and confusion matrix
-6. Exit the program
+6. Make 18 tests to analyze the best approach
+7. Exit the program
 Hint: Enter \"0\" for more detailed description of each menu option.
         """)
 
 
 # Function to display the legend for PartB
-# TODO(adjust as well)
 def mlp_legend():
     print("--------------------------------------------------------------------------------------------------")
     print("""
-1. Reads the 6 columns of data from file partA_input_data.txt and neatly displays it on screen.
-2. Asking user for a limit of laps to search by, then displays only the race results
-which involve that number of home laps or greater, sorted alphabetically by Grand Prix name. 
-3. Calculates the average lap time per race then saves this new information as a
-7th column in file partA_output_data.txt and displays the new data.
-4. Asks the user for a field to sort by and displays on screen all data contained in the file 
-sorted according to the user's instructions (ascending or descending).
-5. Calculates the total average lap time per driver across all Grand Prix races and
-presents it as a GUI column graph in a pop-up window.
-6. Exit the program
+1. Reads the data from file and, parsing it and preparing for future actions. Prints the first 5 lines of the dataframe.
+2. Asking user for a hidden layers topology of the MLP. In case of empty input, make the default reasonable topology.
+3. Asking user for a training step the MLP to adapt with. In case of empty input, make the training step adaptable. 
+4. Splits the data on 80/20 to train on 80% of the data and use the rest of 20% to test the predictions. 
+Shows the progress graph.
+5. Uses the 20% of data to make prediction for classification. Shoes the results, including training report and 
+confusion matrix.
+6. Make a tests of the MLP with 3 different topologies, 3 different learning steps and 2 different training approaches
+to evaluate the best combination out of 18 of them. Saves all the error graphs created into a zip file.
+7. Exit the program
         """)
